782922bbfef457f68bcf291510dbf7763fab9ae2
mykolakdev <kriuchkovmykola@hotmail.com>
2022-09-06 12:38:32 -0400

    card header icon color update

--- src/components/Ui/Card/CardItemUi.vue
+++ src/components/Ui/Card/CardItemUi.vue
@@ -4,7 +4,7 @@
             <!-- card header -->
             <div v-if="iconName && title" class="flex items-center -mt-7">
                 <div
-                    class="bg-gradient-to-r from-gray-600 to-gray-700 w-16 h-16 flex justify-center items-center rounded-sm">
+                    class=" w-16 h-16 bg-gradient-to-r from-gray-600 to-gray-700 text-gray-200 flex justify-center items-center rounded-sm">
                     <IconElementUi :icon-name="iconName" size="1.6" />
                 </div>
                 <div class="ml-4 text-2xl font-semibold text-gray-500">
@@ -19,6 +19,7 @@
 </template>
 
 <script>
+
 import IconElementUi from '../IconElementUi.vue';
 
 export default {
