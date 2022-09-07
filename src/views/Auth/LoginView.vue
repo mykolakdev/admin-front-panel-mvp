5cdc4a23e9cb55c29a3b96d8e9978123534bd7ba
mykolakdev <kriuchkovmykola@hotmail.com>
2022-09-07 10:37:38 -0400

    auth forms inputs

--- src/views/Auth/LoginView.vue
+++ src/views/Auth/LoginView.vue
@@ -1,14 +1,38 @@
 <template>
-    <div>LOGIN</div>
+    <RowUi basis="basis-full">
+        <ColumnUi>
+            <InputUi label="Email" name="email" type="email" :value="email" />
+        </ColumnUi>
+
+        <ColumnUi>
+            <InputUi label="Senha" name="password" type="password" :value="password" />
+        </ColumnUi>
+
+        <ColumnUi>
+            <div class="pt-2 flex justify-between">
+                <InputUi label="Lembre-se de mim" type="checkbox" name="remember_me" />
+
+                <ButtonUi text="Login" icon-name="login" button-style="dark" rounded />
+            </div>
+        </ColumnUi>
+    </RowUi>
 </template>
 
 <script>
 
+import InputUi from '@/components/Ui/Form/InputUi.vue';
+import ButtonUi from '@/components/Ui/ButtonUi.vue';
+import RowUi from '@/components/Ui/Grid/RowUi.vue';
+import ColumnUi from '@/components/Ui/Grid/ColumnUi.vue';
+
 export default {
     name: "LoginView",
+    components: { InputUi, ButtonUi, RowUi, ColumnUi },
 
     data() {
         return {
+            email: null,
+            password: null
         };
     },
 };
--- src/views/Auth/RegisterView.vue
+++ src/views/Auth/RegisterView.vue
@@ -1,11 +1,62 @@
 <template>
-    <div>REGISTER</div>
+    <RowUi basis="basis-full sm:basis-1/2">
+        <ColumnUi>
+            <InputUi label="Nome:" name="fist_name" type="text" :value="first_name" />
+        </ColumnUi>
+
+        <ColumnUi>
+            <InputUi label="Sobrenome:" name="last_name" type="text" :value="last_name" />
+        </ColumnUi>
+
+        <ColumnUi>
+            <InputUi label="Usuário:" name="username" type="text" :value="username" />
+        </ColumnUi>
+
+        <ColumnUi>
+            <InputUi label="Gênero:" name="gender" type="text" :value="gender" />
+        </ColumnUi>
+
+        <ColumnUi basis="basis-full">
+            <InputUi label="Email:" name="email" type="text" :value="email" />
+        </ColumnUi>
+
+        <ColumnUi>
+            <InputUi label="Senha:" name="password" type="text" :value="password" />
+        </ColumnUi>
+
+        <ColumnUi>
+            <InputUi label="Confirmar senha:" name="password_confirmation" type="text" :value="password_confirmation" />
+        </ColumnUi>
+
+        <ColumnUi basis="basis-full">
+            <div class="flex justify-center pt-2">
+                <ButtonUi text="Cadastrar" icon-name="checkLg" button-style="dark" rounded />
+            </div>
+        </ColumnUi>
+    </RowUi>
 </template>
 
 <script>
+import InputUi from '@/components/Ui/Form/InputUi.vue';
+import ButtonUi from '@/components/Ui/ButtonUi.vue';
+import RowUi from '@/components/Ui/Grid/RowUi.vue';
+import ColumnUi from '@/components/Ui/Grid/ColumnUi.vue';
 
 export default {
     name: "RegisterView",
+    components: { InputUi, ButtonUi, RowUi, ColumnUi },
+
+    data() {
+        return {
+            first_name: null,
+            last_name: null,
+            username: null,
+            gender: null,
+            email: null,
+            password: null,
+            password_confirmation: null,
+        };
+    },
 };
 
 </script>
