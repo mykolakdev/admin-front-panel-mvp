206534e1589b661fb201e0c0a1d9746c9ac65877
mykolakdev <kriuchkovmykola@hotmail.com>
2022-09-15 15:01:35 -0400

    Delete user with after delete confirm

--- src/components/Ui/Modal/ModalUi.vue
+++ src/components/Ui/Modal/ModalUi.vue
@@ -58,7 +58,7 @@ export default {
 
     data() {
         return {
-            visible: true,
+            visible: false,
         };
     },
 
--- src/views/UsersListView.vue
+++ src/views/UsersListView.vue
@@ -1,4 +1,10 @@
 <template>
+    <ModalConfirmationUi @confirmAction="deleteUserConfirm" @closeModal="deleteUserCancel"
+        :show="confirmationModal.show" :variant="confirmationModal.variant">
+        <template v-slot:content>
+            <p v-html="confirmationModal.text"></p>
+        </template>
+    </ModalConfirmationUi>
 
     <div class="flex items-center">
         <TitleUi title="Usuários" icon="users" tag="h2" size="h3" />
@@ -7,8 +13,12 @@
                 variant="success" rounded outlined icon="plusLg" />
         </div>
     </div>
+
     <div class="relative py-5">
         <LoadingUi v-if="loading" />
+
+        <AlertUi @alertClose="alert.message = null" class="mb-5" :message="alert.message"
+            :type="alert.variant" />
 
         <ListItemUi v-for="user in users" v-bind:key="user"
             :image-thumb="user.thumb_small">
@@ -35,15 +45,16 @@
                     :to="{name: 'panel.users.edit', params: {'user_id': user.id}}"
                     icon="pencilSquare" size="sm" rounded />
                 <span class="mx-1"></span>
-                <ButtonUi variant="danger" href="#" icon="trash"
-                    size="sm" outlined rounded />
+                <ButtonUi @click.prevent="deleteUserConfirmation" variant="danger"
+                    href="#" icon="trash" size="sm" outlined rounded
+                    :data-user="user.id" />
             </template>
             <!-- /list item actions -->
 
         </ListItemUi>
     </div>
-    <div v-html="pagination">
-    </div>
+
+    <div v-html="pagination"></div>
 </template>
 
 <script>
@@ -54,17 +65,29 @@ import ListItemUi from "@/components/Layout/ListItemUi.vue";
 import ButtonUi from "@/components/Ui/ButtonUi.vue";
 import BadgeUi from "@/components/Ui/BadgeUi.vue";
 import TitleUi from "@/components/Ui/TitleUi.vue";
+import ModalConfirmationUi from "@/components/Ui/Modal/ModalConfirmationUi.vue";
+import AlertUi from "@/components/Ui/AlertUi.vue";
+import messages from "@/utils/messages";
 
 export default {
     name: 'UsersListView',
-    components: { LoadingUi, ListItemUi, ButtonUi, BadgeUi, TitleUi },
+    components: { LoadingUi, ListItemUi, ButtonUi, BadgeUi, TitleUi, ModalConfirmationUi, AlertUi },
 
     data() {
         return {
             loading: true,
             users: [],
+            user: null,
             pagination: null,
-            showConfirmationModal: false
+            confirmationModal: {
+                show: false,
+                text: null,
+                variant: null
+            },
+            alert: {
+                variant: null,
+                message: null,
+            }
         };
     },
 
@@ -82,6 +105,39 @@ export default {
                 console.log(resp);
             });
         },
+
+        deleteUserConfirmation(ev) {
+            let id = ev.target.getAttribute("data-user");
+            this.user = this.users.find(user => user.id == id);
+
+            this.confirmationModal.variant = "info";
+            this.confirmationModal.text = "Tem certeza de que você deseja excluir definitivamente o usuário '<b>" + this.user.full_name + "</b>'?";
+            this.confirmationModal.show = true;
+        },
+
+        deleteUserConfirm() {
+            this.confirmationModal.show = false;
+            this.alert.variant = "success";
+
+            axios.axios.post("/admin/user/destroy/" + this.user.id).then(() => {
+                let index = this.users.findIndex(user => user.id == this.user.id);
+                if (index > -1) {
+                    this.users.splice(index, 1);
+                }
+                this.alert.message = "O usuário " + this.user.full_name + " foi excluído com sucesso definitivamente!";
+                this.alert.variant = "warning";
+            }).catch((resp) => {
+                let errorCode = resp?.response?.data?.error ?? "DefaultMessage";
+
+                this.alert.message = messages[errorCode];
+                this.alert.variant = "danger";
+            });
+        },
+
+        deleteUserCancel() {
+            this.user = null;
+            this.confirmationModal.show = false;
+        }
     },
 
     computed: {
