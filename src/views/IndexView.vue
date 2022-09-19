fb3e2df3dbbcc26ae9b50cd26abfa7c31679025a
mykolakdev <kriuchkovmykola@hotmail.com>
2022-09-18 20:14:04 -0400

    Fill user card

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,65 +1,16 @@
 <template>
 
 	<SectionUi>
-		<RowUi basis="basis-full sm:basis-1/2 lg:basis-1/3">
+		<RowUi basis="basis-full sm:basis-1/2 xl:basis-1/3">
 			<ColumnUi>
 				<CardUi icon-name="users" title="Usuários">
-					<ButtonUi variant="info" icon="list" text="Ver lista" size="sm"
-						rounded outlined class="mr-1 my-1" />
-					<ButtonUi variant="success" icon="plusLg" text="Cadastrar novo"
-						size="sm" rounded class="mr-1 my-1" />
+					<ButtonUi variant="success" :text="usersTotal" size="sm" rounded
+						class="mr-1 my-1" />
+					<ButtonUi variant="info" :text="usersVerified" size="sm" rounded
+						class="mr-1 my-1" />
+					<ButtonUi variant="info" :text="usersUnverified" size="sm" rounded
+						outlined class="mr-1 my-1" />
 				</CardUi>
-			</ColumnUi>
-			<ColumnUi>
-				<CardUi icon-name="layoutWtf" title="Card #2">
-					<ButtonUi variant="dark" text="Html link" icon="linkExternal"
-						size="sm" rounded class="mr-1 my-1"
-						href="https://github.com/ernandesrs" target="_blank"
-						title="Abrir Google em outra aba" />
-					<ButtonUi variant="danger" text="Router link" icon="link" size="sm"
-						rounded outlined class="mr-1 my-1" :to="{name: 'panel.index'}" />
-				</CardUi>
-			</ColumnUi>
-			<ColumnUi>
-				<CardUi icon-name="app" title="Card #3">
-					<ButtonUi variant="info" icon="pencilSquare" text="Button text"
-						size="sm" rounded class="mr-1 my-1" />
-					<ButtonUi variant="warning" icon="alertDiamond" text="Some text"
-						size="sm" rounded outlined class="mr-1 my-1" />
-				</CardUi>
-			</ColumnUi>
-		</RowUi>
-	</SectionUi>
-
-	<SectionUi icon-name="pieChart" title="Uma seção muito top">
-		<RowUi basis="basis-full sm:basis-1/2">
-			<ColumnUi>
-				<p class="mb-4">
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem saepe
-					sit, eos commodi voluptates animi tempore sequi placeat vitae facilis,
-					at cupiditate dolor qui recusandae magnam eveniet aspernatur ducimus
-					quam! Ducimus neque harum cum similique eaque dolores quibusdam est
-					molestias, soluta odit numquam doloremque dolorum blanditiis adipisci
-					vitae amet quam.
-				</p>
-				<ButtonUi variant="dark" text="Large button #1" size="lg" rounded />
-				<span class="mx-1"></span>
-				<ButtonUi variant="info" text="Large button #2" size="lg" rounded />
-			</ColumnUi>
-
-			<ColumnUi>
-				<p class="mb-4">
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem saepe
-					sit, eos commodi voluptates animi tempore sequi placeat vitae facilis,
-					at cupiditate dolor qui recusandae magnam eveniet aspernatur ducimus
-					quam! Ducimus neque harum cum similique eaque dolores quibusdam est
-					molestias, soluta odit numquam doloremque dolorum blanditiis adipisci
-					vitae amet quam.
-				</p>
-				<ButtonUi variant="warning" text="Large button #1" size="lg" rounded />
-				<span class="mx-1"></span>
-				<ButtonUi text="Large button #2" size="lg" rounded
-					outlined />
 			</ColumnUi>
 		</RowUi>
 	</SectionUi>
@@ -73,10 +24,45 @@ import ButtonUi from '@/components/Ui/ButtonUi.vue';
 import RowUi from '@/components/Layout/Grid/RowUi.vue';
 import ColumnUi from '@/components/Layout/Grid/ColumnUi.vue';
 import CardUi from '@/components/Ui/CardUi.vue';
+import axios from '@/services/axios';
 
 export default {
 	name: "IndexView",
 	components: { SectionUi, ButtonUi, RowUi, ColumnUi, CardUi },
+
+	data() {
+		return {
+			users: {
+				total: 0,
+				verified: 0,
+				unverified: 0
+			}
+		};
+	},
+
+	created() {
+		this.getDash();
+	},
+
+	methods: {
+		getDash() {
+			axios.axios.get("/admin").then((resp) => {
+				this.users = resp.data.users;
+			});
+		},
+	},
+
+	computed: {
+		usersTotal() {
+			return `Total: ${this.users?.total}`;
+		},
+		usersVerified() {
+			return `Verificados: ${this.users?.verified}`;
+		},
+		usersUnverified() {
+			return `Não verificados: ${this.users?.unverified}`;
+		},
+	}
 }
 
 </script>
