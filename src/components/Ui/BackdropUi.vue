fd67e108973e600e803f6f53e6db10e7d9e048a3
mykolakdev <kriuchkovmykola@hotmail.com>
2022-09-19 22:02:16 -0400

    Refactoring and updating backdop component

--- src/components/Ui/BackdropUi.vue
+++ src/components/Ui/BackdropUi.vue
@@ -1,10 +1,15 @@
 <template>
-    <Transition enter-from-class="bg-gray-800 opacity-0"
-        enter-active-class="duration-1000" enter-to-class="bg-gray-800 opacity-80"
-        leave-from-class="bg-gray-800 opacity-80" leave-active-class="duration-1000"
-        leave-to-class="bg-gray-800 opacity-0" @after-enter="endAnimation" @after-leave="endAnimation">
-        <div class="w-full h-full absolute z-30 top-0 left-0"
-            :class="{ 'bg-gray-800 opacity-80': showed }">
+    <Transition @after-enter="endAnimation" @after-leave="endAnimation"
+        :enter-from-class="enterFromLeaveToClass" enter-active-class="duration-500"
+        :enter-to-class="leaveFromEnterToClass" :leave-from-class="leaveFromEnterToClass"
+        leave-active-class="duration-500" :leave-to-class="enterFromLeaveToClass">
+        <div :class="['backdrop', fixed?'fixed':'absolute', showedClass]">
+            <div v-if="loading"
+                class="w-full h-full flex flex-col justify-center items-center">
+                <div :class="['spinner', spinner]"></div>
+                <span
+                    :class="[textLoadingClass, 'font-semibold py-2']">{{textLoading}}</span>
+            </div>
         </div>
     </Transition>
 </template>
@@ -12,19 +17,56 @@
 <script>
 
 export default {
-    name: 'BackdropUi',
-
+    name: "BackdropUi",
     data() {
         return {
-            showed: false,
+            showed: false
         };
     },
-
+    props: {
+        light: { type: Boolean, default: false },
+        fixed: { type: Boolean, default: false },
+        loading: { type: Boolean, default: false },
+        textLoading: { type: String, default: 'Aguarde...' },
+    },
     methods: {
         endAnimation() {
             this.showed = !this.showed;
         },
     },
+    computed: {
+        enterFromLeaveToClass() {
+            return `${this.light ? "backdrop-light" : "backdrop-dark"} opacity-0`;
+        },
+        leaveFromEnterToClass() {
+            return `${this.light ? "backdrop-light" : "backdrop-dark"} opacity-80`;
+        },
+        showedClass() {
+            if (this.showed)
+                return this.leaveFromEnterToClass;
+            return null;
+        },
+        spinner() {
+            return `${this.light ? "spinner-light" : "spinner-dark"}`;
+        },
+        textLoadingClass() {
+            return `${this.light ? "text-gray-600" : "text-gray-200"}`;
+        }
+    },
 };
 
 </script>
+
+<style scoped>
+.backdrop {
+    @apply w-full h-full z-30 top-0 left-0
+}
+
+.backdrop-dark {
+    @apply bg-gray-800;
+}
+
+.backdrop-light {
+    @apply bg-gray-100;
+}
+</style>
