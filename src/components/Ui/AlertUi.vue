a2631640011f64e62063b3bdecf7d3a0c87386dc
mykolakdev <kriuchkovmykola@hotmail.com>
2022-09-19 20:20:22 -0400

    Learning and applying animation techniques on alert component

--- src/components/Ui/AlertUi.vue
+++ src/components/Ui/AlertUi.vue
@@ -2,8 +2,8 @@
 
     <Transition @enter="onEnter" @after-enter="afterEnter" @before-leave="beforeLeave"
         @after-leave="afterLeave" enter-from-class="opacity-0"
-        enter-active-class="duration-500" enter-to-class="opacity-100"
-        leave-from-class="opacity-100" leave-active-class="duration-500"
+        enter-active-class="duration-500 ease-out" enter-to-class="opacity-100"
+        leave-from-class="opacity-100" leave-active-class="duration-1000 ease-in-out"
         leave-to-class="opacity-0">
         <div v-if="alertMessage" v-show="visible"
             :class="[{'float-alert': !fixed, 'fixed-alert': fixed}, animation]">
